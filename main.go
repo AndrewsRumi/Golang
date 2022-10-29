3778674fbb8fa488549827a35a4ad96358fa5ff5
AndrewsRumi <rumiandrews@gmail.com>
2022-10-30 03:13:35 +0700

    Selesai

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"pustaka-api/book"
+	"pustaka-api/handler"
+
+	"github.com/gin-gonic/gin"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:200101@tcp(127.0.0.1:3306)/mysql_golang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Db connection error")
+	}
+
+	db.AutoMigrate(&book.Book{})
+
+	bookRepository := book.NewRepository(db)
+
+	bookService := book.NewService(bookRepository)
+
+	bookHandler := handler.NewBookHandler(bookService)
+
+	router := gin.Default()
+
+	v1 := router.Group("/v1")
+
+	v1.GET("/books", bookHandler.GetBooks)
+	v1.GET("/books/:id", bookHandler.GetBook)
+	v1.POST("/books", bookHandler.CreateBook)
+	v1.PUT("/books/:id", bookHandler.UpdateBook)
+	v1.DELETE("/books/:id", bookHandler.DeleteBook)
+
+	router.Run(":2002")
+}
+
+//main
+//handler/book
+//service
+//repository
+//db
+//mysql
